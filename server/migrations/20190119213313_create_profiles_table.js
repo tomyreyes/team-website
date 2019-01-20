d1e81a640ab43a9a84dc6d0570de9e3a71727e20
Tomy <tomyleereyes@gmail.com>
2019-01-19 22:51:58 -0800

    add profiles table

---
+++ server/migrations/20190119213313_create_profiles_table.js
@@ -0,0 +1,17 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('profiles', function(table) {
+    table
+      .increments('id')
+      .unsigned()
+      .primary()
+    table.string('name').nullable()
+    table.string('email').nullable()
+    table.string('bio').nullable()
+    table.dateTime('created_at', 6).defaultTo(knex.fn.now(6))
+    table.dateTime('updated_at', 6).nullable()
+  })
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('profiles')
+}
