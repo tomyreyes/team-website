e836679c27d9ef9455c2547b1721214cd317cd8f
Tomy <tomyleereyes@gmail.com>
2019-01-23 11:24:16 -0800

    add test for relation between user and profile

--- server/tests/unit/users.test.js
+++ server/tests/unit/users.test.js
@@ -1,5 +1,7 @@
 const knex = require('../../db/config')
 const UserHelper = require('../../utils/user')
+const User = require('../../db/models/user')
+const Profile = require('../../db/models/profile')
 
 beforeAll(() => {
   return knex.migrate
@@ -13,13 +15,20 @@ afterAll(() => {
 })
 
 it('User should be found.', () => {
-  const email = 'clarkkent@gmail.com'
+  const email = 'superman@justiceleague.com'
   expect.assertions(1)
   return UserHelper.findEmail(email).then(data => expect(data).toBeTruthy())
 })
 
 it('User should not be found.', () => {
-  const email = 'peterparkergmail.com'
+  const email = 'spiderman@avengers.com'
   expect.assertions(1)
   return UserHelper.findEmail(email).then(data => expect(data).toBeFalsy())
+})
+
+it('Relationship between User and Profile exists.', () => {
+  expect.assertions(1)
+  return new User({ id: 1 }).fetch({ withRelated: 'profile' }).then(user => {
+    expect(user.related('profile').get('user_id')).toBe(1)
+  })
 })
